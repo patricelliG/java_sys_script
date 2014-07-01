fa21856deb18727001eaecf3aab79f5a94cf6328
Samuel Daulton <Sam@dhcp-10-31-200-205.wireless.dartmouth.edu>
2014-07-01 15:53:55 -0400

    win_browser.py

--- win_browser.py
+++ win_browser.py
@@ -15,26 +15,30 @@ else:
     print("IE not found in generic location.")
 
 
+
 '''Pull Data for Firefox Version'''
 #Firefox is dumb and creates a subfolder from a random string.
 #Let's grab that string first'''
 path_ff_lay_1 = "AppData\\Roaming\\Mozilla\\Firefox"
 get_folder = "profiles.ini"
-file_lay_2 = open(usr+"\\"+path_ff_lay_1+"\\"+get_folder)
-regex = re.compile("Path=(.+)")
-lay_2 = regex.findall(file_lay_2.read())
-path_ff_lay_2 = ''.join(lay_2).replace("/","\\")
-#now do same as ie
-config_ff="compatibility.ini"
-filename = usr+"\\"+path_ff_lay_1+"\\"+path_ff_lay_2+"\\"+config_ff
+filename = usr+"\\"+path_ff_lay_1+"\\"+get_folder
 if os.path.exists(filename):
-    file_ff = open(filename).read()
-    regex = re.compile("LastVersion=(.+)")
-    version = regex.findall(file_ff)
-    print("Firefox " + ''.join(version))
+    file_lay_2 = open(filename)
+    regex = re.compile("Path=(.+)")
+    lay_2 = regex.findall(file_lay_2.read())
+    path_ff_lay_2 = ''.join(lay_2).replace("/","\\")
+    #now do same as ie
+    config_ff="compatibility.ini"
+    filename = usr+"\\"+path_ff_lay_1+"\\"+path_ff_lay_2+"\\"+config_ff
+    if os.path.exists(filename):
+        file_ff = open(filename).read()
+        regex = re.compile("LastVersion=(.+)")
+        version = regex.findall(file_ff)
+        print("Firefox " + ''.join(version))
+    else:
+        print("Firefox not found in generic location.")
 else:
     print("Firefox not found in generic location.")
-
 
 '''Pull Data for Chrome Version'''
 path_chrome = "AppData\\Local\\Google\\Chrome\\User Data"
