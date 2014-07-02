00e926609739bf3d286fe3c429f5d9e6ba148d0b
Sam Daulton <sdaulton8@gmail.com>
2014-07-02 13:47:14 -0400

    updates

--- mac_browser.py
+++ mac_browser.py
@@ -1,61 +1,57 @@
-
+import lib
 import os
 import re
-usr = os.path.expanduser("~")
 
-'''Pull Data for Safari Verson'''
-path_safari = "/Applications/Safari.app/Contents"
-config_safari = "version.plist"
-filename = path_safari+"/"+config_safari
-if os.path.exists(filename):
-    file_safari = open(filename)
-    text_safari = file_safari.read()
-    file_safari.close()
-    regex = re.compile("<key>CFBundleShortVersionString</key>\n[\s+]<string>([\d|.]+)")
-    version = regex.findall(text_safari)
-    print("Safari "+ ''.join(version))
-else:
-    print("Safari not found in generic location.")
+def browser_version(output):
+'''
+Reads the machine's current browsers' versions and writes them to the output
+file given as a parameter
+'''
 
+    usr = os.path.expanduser("~")
 
-'''Pull Data for Firefox Version'''
-#Firefox is dumb and creates a subfolder from a random string.
-#Let's grab that string first'''
-path_ff_lay_1 = "Library/Application Support/Firefox"
-get_folder = "profiles.ini"
-filename = usr+"/"+path_ff_lay_1+"/"+get_folder
-if os.path.exists(filename):
-    file_lay_2 = open(filename)
-    regex = re.compile("Path=(.+)")
-    lay_2 = regex.findall(file_lay_2.read())
-    file_lay_2.close()
-    path_ff_lay_2 = ''.join(lay_2)
-    #now do same as ie
-    config_ff="compatibility.ini" 
-    filename = usr+"/"+path_ff_lay_1+"/"+path_ff_lay_2+"/"+config_ff
+    '''Pull Data for Safari Verson'''
+    path_safari = "/Applications/Safari.app/Contents"
+    config_safari = "version.plist"
+    filename = path_safari+"/"+config_safari
     if os.path.exists(filename):
-        file_ff = open(filename)
-        text_ff = file_ff.read()
-        file_ff.close()
-        regex = re.compile("LastVersion=(.+)")
-        version = regex.findall(text_ff)
-        print("Firefox " + ''.join(version))
+        regex = re.compile("<key>CFBundleShortVersionString</key>\n[\s+]<string>([\d|.]+)")
+        version = lib.extract_from_file(filename, regex)
+        output.write("Safari "+ ''.join(version))
     else:
-        print("Firefox not found in generic location.")
-else:
-    print("Firefox not found in generic location.")
+        output.write("Safari not found in generic location.")
 
-'''Pull Data for Chrome Version'''
-path_chrome = "Library/Application Support/Google/Chrome"
-config_chrome = "Local State"
-filename = usr+"/"+path_chrome+"/"+config_chrome
-if os.path.exists(filename):
-    file_chrome = open(filename)
-    text_chrome = file_chrome.read()
-    file_chrome.close()
-    regex = re.compile("\"stats_version\": \"([\d|.]+)\"")
-    version = regex.findall(text_chrome)
-    print("Chrome "+ ''.join(version))
-else:
-    print("Chrome not found in generic location.")
+
+    '''Pull Data for Firefox Version'''
+    #Firefox is dumb and creates a subfolder from a random string.
+    #Let's grab that string first'''
+    path_ff_lay_1 = "Library/Application Support/Firefox"
+    get_folder = "profiles.ini"
+    filename = usr+"/"+path_ff_lay_1+"/"+get_folder
+    if os.path.exists(filename):
+        regex = re.compile("Path=(.+)")
+        lay_2 = lib.extract_from_file(filename, regex)
+        path_ff_lay_2 = ''.join(lay_2)
+        #now do same as ie
+        config_ff="compatibility.ini" 
+        filename = usr+"/"+path_ff_lay_1+"/"+path_ff_lay_2+"/"+config_ff
+        if os.path.exists(filename):
+            regex = re.compile("LastVersion=(.+)")
+            version = lib.extract_from_file(filename, regex)
+            output.write("Firefox " + ''.join(version))
+        else:
+            output.write("Firefox not found in generic location.")
+    else:
+        output.write("Firefox not found in generic location.")
+
+    '''Pull Data for Chrome Version'''
+    path_chrome = "Library/Application Support/Google/Chrome"
+    config_chrome = "Local State"
+    filename = usr+"/"+path_chrome+"/"+config_chrome
+    if os.path.exists(filename):
+        regex = re.compile("\"stats_version\": \"([\d|.]+)\"")
+        version = lib.extract_from_file(filename, regex)
+        output.write("Chrome "+ ''.join(version))
+    else:
+        output.write("Chrome not found in generic location.")
 
--- mac_default_browser.py
+++ mac_default_browser.py
@@ -2,79 +2,71 @@
 
 import os.path
 
-def mac_browser():
+def mac_browser(output):
     '''
     Pulls the file and prints the default browser for various file types
     '''
     user = os.path.expanduser('~')
     filename = user + '/Library/Preferences/com.apple.LaunchServices.plist'
     if not (os.path.exists(filename)):
-        print filename + " does not exist"
+        output.write(filename + " does not exist, so we could not get the current browser")
         return
     preferences_file = open(filename, 'r')
     preferences = preferences_file.read()
     preferences_file.close()
     #print preferences.split('\xd2')
 
-    print "Default Browsers:"
+    output.write("Default Browsers:")
     #Thttp
     type_idx = preferences.find('Thttp')
     if type_idx == -1:
-        print "Thttp: Default browser not found"
+        output.write("Thttp: Default browser not found")
     else:
         this_preference = preferences[type_idx:]
         start = this_preference.find('com') + 4
         end = this_preference.find ('\xd2')
 
-        print "Thttp: " + this_preference[start:end]
+        output.write("Thttp: " + this_preference[start:end])
 
     #Uhttps
     type_idx = preferences.find('Uhttps')
     if type_idx == -1:
-        print "Uhttps: Default browser not found"
+        output.write("Uhttps: Default browser not found")
     else:
         this_preference = preferences[type_idx:]
         start = this_preference.find('com') + 4
         end = this_preference.find ('\xd2')
 
-        print "Uhttps: " + this_preference[start:end]
+        output.write("Uhttps: " + this_preference[start:end])
 
     #public.html
     type_idx = preferences.find('public.html')
     if type_idx == -1:
-        print "Public html: Default browser not found"
+        output.write("Public html: Default browser not found")
     else:
         this_preference = preferences[type_idx:]
         start = this_preference.find('com') + 4
         end = this_preference.find ('\xd2')
 
-        print "Public html: " + this_preference[start:end]
+       output.write("Public html: " + this_preference[start:end])
 
     #public.url
     type_idx = preferences.find('public.url')
     if type_idx == -1:
-        print "Public url: Default browser not found"
+        output.write("Public url: Default browser not found")
     else:
         this_preference = preferences[type_idx:]
         start = this_preference.find('com') + 4
         end = this_preference.find ('\xd2')
-        print "Public url: " + this_preference[start:end]
+        output.write("Public url: " + this_preference[start:end])
 
     #public.xhtml
     type_idx = preferences.find('public.xhtml')
     if type_idx == -1:
-        print "Public xhtml: Default browser not found"
+        output.write("Public xhtml: Default browser not found")
     else:
         this_preference = preferences[type_idx:]
         start = this_preference.find('com') + 4
         end = this_preference.find ('\xd3')
-        print "Public xhtml: " + this_preference[start:end]
-        
-
-    
-        
-    
-
-mac_browser()
-    
+        output.write("Public xhtml: " + this_preference[start:end])
     
