49b9e017dd1987cbe17b2c0aa309118e2f71367e
Gary Patricelli <garypatricelli@gmail.com>
2014-07-02 09:17:26 -0400

    init scan_java.py

--- scan_java.py
+++ scan_java.py
@@ -1,16 +1,23 @@
 import os 
+import deploy_prop.py
+
 #baseDir = os.path.expanduser("~")
 #file = baseDir+"/Documents"
 #print(file)
 #if(os.path.exists(file)):
 
-# Get Java Settings
+# Get User level Java Settings
+
+# Check if the file exists
 baseDir = os.path.expanduser("~")
-dep_prop_file = baseDir+"AppData\LocalLow\Sun\Java\Deployment\deployment.properties"
+deployment_prop_file = baseDir+"AppData\LocalLow\Sun\Java\Deployment\deployment.properties"
+if(os.path.exists(deployment_prop_file)):
+    # File exists, read in the settings
+else
+    # File not found, print error
+    print("ERROR: User level deployment.properties file not found."
  
-# Build data structure of default deployment.property values
-dep_prop = { 'deployment.user.cachedir', ("\"$USER_HOME\" + File.separator + \"cache\"", "User-level cache directory."),
-             'deployment.system.cachedir', ("null", "\"System-level cache directory.\"") }
+
 
 
 
