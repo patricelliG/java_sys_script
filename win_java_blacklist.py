85408c7b8a963c87d63c2b7915d31b1b764a4b3f
Sam Daulton <sdaulton8@gmail.com>
2014-07-02 15:22:18 -0400

    Add blacklist/whitelist for windows

---
+++ win_java_blacklist.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+'''
+Reads Java Blacklist file and writes its contents to the output file taken
+as a parameter
+'''
+
+def blacklist(output):
+
+    usr = os.path.expanduser("~")
+
+    '''Pull Data for Exception Sites'''
+    path_blacklist = "AppData\\LocalLow\\Sun\\Java\\Deployment\\security"
+    config_blacklist = "blacklist.dynamic"
+    filename = usr + "\\" + path_blacklist+"\\"+config_blacklist
+    if os.path.exists(filename):
+        file_blacklist = open(filename)
+        blacklist = file_blacklist.read()
+        file_blacklist.close()
+        if blacklist == '':
+            output.write("NONE")
+        else:
+            output.write(blacklist)
+    else:
+        output.write("Java Blacklisted Sites not found in generic location.")
---
+++ win_java_exception_sites.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+
+def exception_sites(output):
+    '''
+Reads Java Exception Sites and writes those sites to the output file given
+as a parameter
+'''
+
+    usr = os.path.expanduser("~")
+
+    '''Pull Data for Exception Sites'''
+    path_exception_sites = "AppData\\LocalLow\\Sun\\Java\\Deployment\\security"
+    config_exception_sites = "exception.sites"
+    filename = usr + "\\" + path_exception_sites+"\\"+config_exception_sites
+    if os.path.exists(filename):
+        file_exception_sites = open(filename)
+        exception_sites = file_exception_sites.read()
+        file_exception_sites.close()
+        if exception_sites == '':
+            output.write("NONE")
+        else:
+            output.write(exception_sites)
+    else:
+        output.write("Java Exception Sites not found in generic location.")
