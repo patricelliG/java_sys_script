7234958ff243851f1a4725ce2f068e369d044737
Michael Millian <xenakaaii@gmail.com>
2014-07-02 16:16:54 -0400

    Added Admin settings detection

--- win_deploy_prop.py
+++ win_deploy_prop.py
@@ -1,13 +1,9 @@
 import os
 import re
-
 from lib import extract_from_file
-
 
 def run_win_deploy_prop (output):
     # Get User level Java Settings
-    
-    # Check if the file exists
     usr = os.path.expanduser("~")
     path_deployment_prop = "AppData\LocalLow\Sun\Java\Deployment"
     config_deployment_prop = "deployment.properties"
@@ -19,16 +15,10 @@ def run_win_deploy_prop (output):
             output.write(line), #this is the weirdest syntax to remove newlines
     else:
         output.write("ERROR: User level deployment.properties file not found.\n")
-    
-'''Admin Lvl'''
-'''config_deployment_conf = "deployment.config"
-filename = usr+"\\"+path_deployment_prop+"\\"+config_deployment_conf
-print(filename)
-regex = re.compile("deployment.system.config=file\\\\:(.+)")
-path_unformatted = extract_from_file(filename, regex)
-path_formatted = ''.join(path_unformatted).replace('/','\\').replace('C\\','C')
-print(path_formatted)
-if os.path.exists(path_formatted):
-    print("yes")
-'''
+ 
+    # Get Admin Level Settings 
+	config_deployment_conf = "deployment.config"
+	filename = usr+"\\"+path_deployment_prop+"\\"+config_deployment_conf
+	if os.path.exists(filename):
+		output.write("Advanced Administrator settings indicated. For settings location see "+filename+"\n")
 
